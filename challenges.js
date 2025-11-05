362510b12a94e6f92964f6b8261d4389250b746c
KyraWynne9 <kewynne@email.neit.edu>
2025-11-05 11:07:16 -0500

    a

--- challenges.js
+++ challenges.js
@@ -1,6 +1,12 @@
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 
+function gameLoop() {
+
+}
+
+setInterval(gameLoop, 50);
+
 function raiseScore() {
     score += 1;
     let scoreElement = document.getElementById("scoreDisplay");
@@ -8,4 +14,8 @@ function raiseScore() {
 
 }
 
-setInterval(gameLoop, 50);
+function detectClick() {
+    console.log("Clicked!")
+}
+
+canvas.addEventListener("CLick", detectClick);
